c8ad68d2c5c0500a38100912d603759a49c4e7a4
李貌 <limao@zcool.com.cn>
2022-05-22 18:29:44 +0800

    fix bug

--- 7.9/js.js
+++
@@ -1,4 +0,0 @@
-var name = prompt('生活百般滋味，人生笑来面对。\n大家好!我的名字是','张佳慧')
-var dream = prompt('我的梦想是','可以和队友们一起创业')
-var conpany = prompt('你有什么故事','我有人')
-document.write('生活百般滋味，人生笑来面对。\n大家好!我的名字是<a href="">'+name+'</a><br>我的梦想是<a href="">'+dream+'</a><br><a href="">'+conpany+'</a>')
